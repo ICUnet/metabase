2feb0b8c5ef62f12f6675a90128bfee86f77b10f
Timofei <timofei@metabase.com>
2025-11-05 02:47:06 +0400

    Open Static Legacy modal from EmbedJS wizard Alert

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/components/LegacyStaticEmbeddingAlert.tsx
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/components/LegacyStaticEmbeddingAlert.tsx
@@ -1,12 +1,20 @@
+import { push } from "react-router-redux";
 import { c, t } from "ttag";
 
+import { STATIC_LEGACY_EMBEDDING_TYPE } from "metabase/embedding/constants";
+import { useDispatch } from "metabase/lib/redux";
+import * as Urls from "metabase/lib/urls";
+import { setOpenModal } from "metabase/redux/ui";
 import { Alert, Anchor, Box, Flex, Icon, Stack, Text } from "metabase/ui";
 import { useSdkIframeEmbedSetupContext } from "metabase-enterprise/embedding_iframe_sdk_setup/context";
 import { getResourceTypeFromExperience } from "metabase-enterprise/embedding_iframe_sdk_setup/utils/get-resource-type-from-experience";
+import type { Card, Dashboard } from "metabase-types/api";
 
 export const LegacyStaticEmbeddingAlert = () => {
-  const { settings, resource, experience } = useSdkIframeEmbedSetupContext();
+  const { settings, resource, experience, onClose } =
+    useSdkIframeEmbedSetupContext();
 
+  const dispatch = useDispatch();
   const isStaticEmbedding = !!settings.isStatic;
 
   const resourceType = getResourceTypeFromExperience(experience);
@@ -41,7 +49,25 @@ export const LegacyStaticEmbeddingAlert = () => {
             {t`This embed uses the legacy static embedding method. The controls shown are for the new embedding method, which is recommended.`}
           </Text>
 
-          <Anchor key="anchor" fw="bold" lh="lg">
+          <Anchor
+            key="anchor"
+            fw="bold"
+            lh="lg"
+            onClick={() => {
+              onClose();
+
+              dispatch(
+                push({
+                  pathname:
+                    resourceType === "dashboard"
+                      ? Urls.dashboard({ id: (resource as Dashboard).id })
+                      : Urls.question({ id: (resource as Card).id }),
+                }),
+              );
+
+              dispatch(setOpenModal(STATIC_LEGACY_EMBEDDING_TYPE));
+            }}
+          >
             {c("A link that toggles the legacy static embedding wizard.")
               .t`Use legacy static embedding (not recommended)`}
           </Anchor>
